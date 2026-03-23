9d512f9dc530328e3d84e4a22605255227e56390
Manik Manavenddra M <156107867+Manik0107@users.noreply.github.com>
2026-03-23 20:14:18 +0530

    Time: 126 ms (43.57%) | Memory: 31.1 MB (75.69%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -1,25 +1,22 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        ple = [-1] * len(heights)
-        n = len(heights)
         stack = []
-        for i in range(n):
-            while stack and heights[stack[-1]] >= heights[i]:
+        maxarea = 0
+        for i in range(len(heights)):
+            while stack and heights[stack[-1]] > heights[i]:
+                ele = heights[stack[-1]]
                 stack.pop()
-            ple[i] = stack[-1] if stack else -1
-            stack.append(i)
+                nse = i
+                pse = stack[-1] if stack else -1
+                maxarea = max(maxarea, ele * (nse - pse - 1))
 
-        nle = [n] * n
-        stack = []
-        for i in range(n-1, -1,-1):
-            while stack and heights[stack[-1]] >= heights[i]:
-                stack.pop()
-            nle[i] = stack[-1] if stack else n
             stack.append(i)
+        while stack:
+            ele = heights[stack[-1]]
+            stack.pop()
+            nse = len(heights)
+            pse = stack[-1] if stack else -1
+            maxarea = max(maxarea, ele * (nse - pse - 1))
 
-        maxi = heights[0]
-        for i in range(n):
-            length = nle[i] - ple[i] - 1
-            maxi = max(maxi, heights[i] * length)
+        return maxarea
         
-        return maxi
